dd170e9d3b5d3ac8756986dc139248f9334ce8d9
Migo <miguelvalencia2497@gmail.com>
2017-08-28 15:45:31 +0800

    Added to do state with dynamic output

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,35 @@
-alert('it workssss');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ToDoComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ['watch rick and morty', 'learn react', 'play dota', 'sleep']
+    }
+  }
+
+
+
+  render() {
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return(
+        <li>{item}</li>
+      );
+    });
+    return(
+      <div id="todo-list">
+        <p>Things to do today</p>
+        <ul>
+          {todos}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ToDoComponent />,
+  document.getElementById('todo-wrapper')
+);
